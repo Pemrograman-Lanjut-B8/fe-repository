6745ca2db68bd1d815be1de815510acf6e9143f9
novrizair <novrizal.airsyahputra@ui.ac.id>
2024-05-26 22:10:39 +0700

    [REFACTOR] Edit dimas baseurl

--- actions/bookApi.ts
+++ actions/bookApi.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { Book } from '@/types/book';
 import AuthService from '@/app/services/auth.service';
 
-const BASE_URL = 'http://34.124.134.197/api/book';
+const BASE_URL = 'http://34.101.227.179/api/book';
 
 export const getAllBooks = async () => {
     try {
